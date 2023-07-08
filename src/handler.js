5fee55be5a4694493321b6c8f6b07e5956fc4bb6
John Doe <johndoe@example.com>
2023-07-08 16:31:54 +0700

    mandatory done

--- src/handler.js
+++ src/handler.js
@@ -2,22 +2,14 @@ const {nanoid} = require('nanoid');
 const books = require('./books');
 
 const addBookHandler = (request, h) => {
-  const {name, year, author, summary, publisher, pageCount, readPage,
-    reading} = request.payload;
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
 
   const id = nanoid(16);
   const finished = pageCount === readPage ? true : false;
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
-
-  const newBook = {
-    name, year, author, summary, publisher, pageCount, readPage, reading,
-    id, finished, insertedAt, updatedAt,
-  };
-
-  books.push(newBook);
-
-  const isSuccess = books.filter((book) => book.id === id).length > 0;
 
   if (!name) {
     const response = h.response({
@@ -34,7 +26,16 @@ const addBookHandler = (request, h) => {
     });
     response.code(400);
     return response;
-  } else if (isSuccess) {
+  }
+
+  const newBook = {
+    id, name, year, author, summary, publisher, pageCount, readPage, finished,
+    reading, insertedAt, updatedAt,
+  };
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+  if (isSuccess) {
     const response = h.response({
       status: 'success',
       message: 'Buku berhasil ditambahkan',
@@ -44,7 +45,7 @@ const addBookHandler = (request, h) => {
     });
     response.code(201);
     return response;
-  }
+  };
 
   const response = h.response({
     status: 'fail',
@@ -71,4 +72,102 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = {addBookHandler, getAllBooksHandler};
+const getBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+
+  if (book !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+    response.code(200);
+    return response;
+  };
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const editBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    if (!name) {
+      const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Mohon isi nama buku',
+      });
+      response.code(400);
+      return response;
+    } else if (readPage > pageCount) {
+      const response = h.response({
+        status: 'fail',
+        // eslint-disable-next-line max-len
+        message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+      });
+      response.code(400);
+      return response;
+    };
+    books[index] = {
+      ...books[index], name, year, author, summary, publisher, pageCount,
+      readPage, reading, updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  };
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  };
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler,
+  deleteBookByIdHandler,
+};
